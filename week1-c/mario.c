4f42e68f61d317e8c578bf9fdaf2059edbec6452
Ujenyuojo Egwuda <83659965+Ujenyhu@users.noreply.github.com>
2026-07-12 22:37:05 +0000

    Sun, Jul 12, 2026, 11:37 PM +01:00

--- week1-c/mario.c
+++ week1-c/mario.c
@@ -5,6 +5,10 @@ int get_pyramid_height(void);
 void render_pyramid(int height);
 void print_characters(char character, int count);
 
+//Bind user's valid height values to [1, 8] inclusive
+const int minHeight = 1;
+const int maxHeight = 8;
+
 int main(void)
 {
     int height = get_pyramid_height();
@@ -12,4 +16,26 @@ int main(void)
     render_pyramid(height);
 
     return 0;
+}
+
+
+int get_pyramid_height(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < minHeight || height > maxHeight)
+
+    return height;
+}
+
+
+void render_pyramid(int height)
+{
+    for(int row = 1; row <= height; row++)
+    {
+
+    }
 }
