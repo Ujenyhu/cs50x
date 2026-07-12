aafcda2c9260a7ca76ef7ec32c81ddfe68902d23
Ujenyuojo Egwuda <83659965+Ujenyhu@users.noreply.github.com>
2026-07-12 23:25:49 +0000

    Mon, Jul 13, 2026, 12:25 AM +01:00

--- week1-c/credit.c
+++ week1-c/credit.c
@@ -6,3 +6,33 @@ bool validate_luhn(long card_number);
 int get_card_length(long card_number);
 int get_leading_digits(long card_number, int digit_count);
 void classify_network(int length, int prefix);
+
+// American Express uses 15-digit numbers, MasterCard uses 16-digit numbers, and Visa uses 13- and 16-digit numbers.
+const int min_length = 13;
+const int max_length =16;
+
+int main(void)
+{
+    long card_number = get_long("Number: ");
+
+    // Validate Length
+    int length = get_card_length(card_number);
+    if (length < min_length || length > max_length)
+    {
+        printf("INVALID\n");
+        return 0;
+    }
+
+    // Luhn Validation
+    if (!validate_luhn(card_number))
+    {
+        printf("INVALID\n");
+        return 0;
+    }
+
+    // 4. Prefix Extraction and Final Network Resolution
+    int prefix = get_leading_digits(card_number, length);
+    classify_network(length, prefix);
+
+    return 0;
+}
