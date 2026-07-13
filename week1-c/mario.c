f15f5104466f1bff60c0e4a61d59a08d78d9a334
Ujenyuojo Egwuda <83659965+Ujenyhu@users.noreply.github.com>
2026-07-13 02:30:27 +0000

    Mon, Jul 13, 2026, 3:30 AM +01:00

---
+++ week1-c/mario.c
@@ -0,0 +1,60 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_pyramid_height(void);
+void render_pyramid(int height);
+void print_characters(char character, int count);
+
+// valid height should be [1, 8] inclusive
+const int MIN_HEIGHT = 1;
+const int MAX_HEIGHT = 8;
+
+int main(void)
+{
+    int height = get_pyramid_height();
+
+    render_pyramid(height);
+
+    return 0;
+}
+
+int get_pyramid_height(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < MIN_HEIGHT || height > MAX_HEIGHT);
+
+    return height;
+}
+
+void render_pyramid(int height)
+{
+    for (int row = 1; row <= height; row++)
+    {
+        // indentation spaces on the left start
+        int spaces = height - row;
+        print_characters(' ', spaces);
+
+        // Left pyramid segment
+        print_characters('#', row);
+
+        // The divider gap
+        printf("  ");
+
+        // Right segment
+        print_characters('#', row);
+
+        printf("\n");
+    }
+}
+
+void print_characters(char character, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        printf("%c", character);
+    }
+}
---
+++ week2-arrays/scrabble.c
@@ -0,0 +1,54 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet from A to Z
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
+
+    return 0;
+}
+
+int compute_score(string word)
+{
+    int score = 0;
+
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        if (isupper(word[i]))
+        {
+            // Normalize character to an array index between 0 and 25
+            score += POINTS[word[i] - 'A'];
+        }
+        else if (islower(word[i]))
+        {
+            // Normalize character to an array index between 0 and 25
+            score += POINTS[word[i] - 'a'];
+        }
+    }
+
+    return score;
+}
