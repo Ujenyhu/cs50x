a3d083a687dfed20f15afd7e6da0aebbc89e9c21
Ujenyuojo Egwuda <83659965+Ujenyhu@users.noreply.github.com>
2026-07-18 15:21:00 +0000

    Sat, Jul 18, 2026, 4:21 PM +01:00

---
+++ week2-arrays/substitution.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+// Status Codes
+const int STATUS_SUCCESS = 0;
+const int STATUS_ERROR = 1;
+
+//Cryptographic const
+const int KEY_LENGTH = 26;
