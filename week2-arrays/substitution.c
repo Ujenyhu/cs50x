d837630070a985e91c24afd6adab5b59d35d2266
Ujenyuojo Egwuda <83659965+Ujenyhu@users.noreply.github.com>
2026-07-18 16:58:16 +0000

    Sat, Jul 18, 2026, 5:58 PM +01:00

--- week2-arrays/substitution.c
+++ week2-arrays/substitution.c
@@ -18,7 +18,7 @@ const int ERROR_DUPLICATE = 3;
 
 
 int validate_key(string key);
-void encrypt_text(string plaintext, string key);
+string substitute_text(string plaintext, string key);
 
 int main(int argc, string argv[])
 {
@@ -48,8 +48,8 @@ int main(int argc, string argv[])
 
     string plaintext = get_string("plaintext:  ");
 
-    encrypt_text(plaintext, argv[1]);
-    printf("ciphertext: %s\n", plaintext);
+    string ciphertext = substitute_text(plaintext, argv[1]);
+    printf("ciphertext: %s\n", ciphertext);
 
     return STATUS_SUCCESS;
 }
@@ -95,12 +95,12 @@ int validate_key(string key)
 ** enfoce case-sensitivity. Must be in the smae case as the plaintext
 ** Maintain symbols, punctuation, and white spaces. No mutation
 */
-void encrypt_text(string plaintext, string key)
+string encrypt_text(string plaintext, string key)
 {
     for (int i = 0, n = strlen(plaintext); i < n; i++)
     {
         char target = plaintext[i];
-
+        
         if (isupper(target))
         {
             int index = target - 'A';
@@ -112,4 +112,6 @@ void encrypt_text(string plaintext, string key)
             plaintext[i] =  tolower(key[index]);
         }
     }
+
+    return plaintext;
 }
